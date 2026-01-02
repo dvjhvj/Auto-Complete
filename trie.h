5d7ce0443f6910247aa79de80a955e4e71b1d8a1
lsnk_ute <nhatkhue2006@gmail.com>
2026-01-02 16:39:15 +0700

    Update trie.h

--- trie.h
+++ trie.h
@@ -7,39 +7,28 @@
 
 using namespace std;
 
-/*
- * TrieNode represents a single character in the Trie.
- * Children are stored in a hash table (secure against hash flooding).
- */
 struct TrieNode {
-    bool isEnd;                         // true if this node ends a word
-    HashTable<char, TrieNode*> children;
+    bool isEnd;
+    HashTable children;
+    vector<char> childrenKeys; // để DFS
 
     TrieNode();
 };
 
-/*
- * Trie data structure for secure auto-complete
- */
 class Trie {
 private:
     TrieNode* root;
 
-    // DFS helper function for autocomplete
     void dfs(TrieNode* node,
              string current,
              vector<string>& result);
 
-    // Find the node corresponding to a prefix
     TrieNode* searchPrefix(const string& prefix);
 
 public:
     Trie();
 
-    // Insert a word into the Trie
     void insert(const string& word);
-
-    // Return all words starting with prefix
     vector<string> autocomplete(const string& prefix);
 };
 
