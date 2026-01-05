f0dfa9ccc11fde2fb6640d4b99f8efba92fd1cd2
24110099-dot <24110099@student.hcmute.edu.vn>
2026-01-05 10:24:15 +0700

    Refactor HashNode structure and comments in hash_table.h

--- hash_table.h
+++ hash_table.h
@@ -1,28 +1,32 @@
+/*
+FullName: NGUYEN QUOC BAO KHANG
+ID Student: 24110099
+Purpose: Hash_table.h
+Date: Jan 2nd, 2025.
+*/
+
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
 #include "universal_hash.h"
-
-// Forward declaration of TrieNode (Member B's struct)
-// We don't need the full definition here, just the name.
-struct TrieNode; 
+struct TrieNode;
 
 // Bucket Node for Separate Chaining [cite: 29]
 struct HashNode {
     char key;
-    TrieNode* value; // The child node in the Trie
-    HashNode* next;  // Pointer to next entry if collision occurs
+    TrieNode* value;
+    HashNode* next; 
 };
 
 class HashTable {
 private:
-    HashNode** buckets;      // Array of pointers to HashNodes
-    int capacity;            // Size of the array
-    UniversalHash* hasher;   // The security component
+    HashNode** buckets;      
+    int capacity;            
+    UniversalHash* hasher;  
     int collisionCount;      // Statistic for your report [cite: 34]
 
 public:
-    HashTable(int size = 8); // Default size 8 is enough for sparse nodes
+    HashTable(int size = 8); 
     ~HashTable();
 
     // Interface required by Member B [cite: 8]
