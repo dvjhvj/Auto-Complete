6af0e335f2c0a3d8975f87b326d8163e257eb708
24110099-dot <24110099@student.hcmute.edu.vn>
2026-01-05 09:57:37 +0700

    hash_table.cpp

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,15 +1,22 @@
+/*
+FullName: NGUYEN QUOC BAO KHANG
+ID Student: 24110099
+Purpose: Hash_table
+Date: Jan 2nd, 2025.
+*/
+
 #include "hash_table.h"
 #include <cstddef> // for NULL/nullptr
 
 HashTable::HashTable(int size) {
     this->capacity = size;
     this->collisionCount = 0;
-    
+
     // Create the secure hasher for this specific table
     this->hasher = new UniversalHash(capacity);
 
     // Initialize buckets to nullptr
-    this->buckets = new HashNode*[capacity];
+    this->buckets = new HashNode * [capacity];
     for (int i = 0; i < capacity; i++) {
         buckets[i] = nullptr;
     }
@@ -29,34 +36,34 @@ HashTable::~HashTable() {
     delete hasher;
 }
 
-void HashTable::insert(char key, TrieNode* node) { // [cite: 31]
+void HashTable::insert(char key, TrieNode* node) {
     int index = hasher->hash(key);
-    
+
     HashNode* entry = buckets[index];
-    
+
     // Check if bucket is empty
     if (entry == nullptr) {
-        buckets[index] = new HashNode{key, node, nullptr};
+        buckets[index] = new HashNode{ key, node, nullptr};
         return;
     }
 
-    // Bucket not empty -> Collision or Update
+    // Bucket not empty
     HashNode* prev = nullptr;
     while (entry != nullptr) {
         if (entry->key == key) {
-            entry->value = node; // Update existing key
+            entry->value = node;
             return;
         }
         prev = entry;
         entry = entry->next;
     }
 
-    // Key not found in chain, append new node (Collision handled)
-    prev->next = new HashNode{key, node, nullptr};
-    this->collisionCount++; // Track for report [cite: 34]
+    // Key not found in chain, append new node
+    prev->next = new HashNode{ key, node, nullptr };
+    this->collisionCount++;
 }
 
-TrieNode* HashTable::find(char key) { // [cite: 32]
+TrieNode* HashTable::find(char key) { 
     int index = hasher->hash(key);
     HashNode* entry = buckets[index];
 
